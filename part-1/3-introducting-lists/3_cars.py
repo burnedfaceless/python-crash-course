52a21844fecbdd32711fbb0f90b17020e258516d
Brian Abbott <hi@brianabbott.com>
2023-12-16 15:51:44 -0500

    created 3_cars.py

---
+++ part-1/3-introducting-lists/3_cars.py
@@ -0,0 +1,32 @@
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+# sort by alphabetical order - changes the array
+cars.sort()
+print(cars)
+
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+# sort in reverse alphabetical order
+cars.sort(reverse=True)
+print(cars)
+
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+
+print('Here is the original list:')
+print(cars)
+
+print('\nHere is the sorted list:')
+# sort without changing the list
+print(sorted(cars))
+
+print('\nHere is the original list again:')
+print(cars)
+
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print(cars)
+
+cars.reverse()
+print(cars)
+
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+length_of_list = len(cars)
+
+print(length_of_list)
