038f119d664e3648f72a87ec106528152971060b
Brian Abbott <hi@brianabbott.com>
2023-12-16 19:59:50 -0500

    created 3_first_numbers.py

---
+++ part-1/4-working-with-lists/3_first_numbers.py
@@ -0,0 +1,16 @@
+for value in range(1, 5):
+    print(value)
+
+print('\n')
+
+for value in range(1, 6):
+    print(value)
+
+print('\n')
+
+for value in range(6):
+    print(value)
+
+numbers = list(range(1, 6))
+print(numbers)
+
