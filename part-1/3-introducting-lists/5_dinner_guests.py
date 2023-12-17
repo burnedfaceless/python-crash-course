dc6fa40779e1526d6e88938509280a8d71a88a85
Brian Abbott <hi@brianabbott.com>
2023-12-16 19:06:03 -0500

    created 5_dinner_guests

---
+++ part-1/3-introducting-lists/5_dinner_guests.py
@@ -0,0 +1,5 @@
+dinner_guests = ['Brian Abbott', 'Carolyn Gunby', 'Rob Gunby', 'Hannah Abbott', 'Logan Abbott', 'Tony Abbott', 'Carol Swinford', 'Laura Abbott']
+
+number_of_dinner_guests = len(dinner_guests)
+
+print(f'We are having {number_of_dinner_guests} guests over for Christmas dinner!')
