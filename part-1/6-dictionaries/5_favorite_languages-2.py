52153c7709d82be125d690a07e51c962e68b9180
Brian Abbott <hi@brianabbott.com>
2023-12-19 16:58:36 -0500

    created 5_favorite_languages-2.py

--- part-1/6-dictionaries/5_favorite_languages-2.py
+++ part-1/6-dictionaries/5_favorite_languages-2.py
@@ -7,4 +7,17 @@ favorite_languages = {
 
 for name, language in favorite_languages.items():
     print(f'{name.title()}\'s favorite language is {language.title()}')
-    
+
+for name in favorite_languages.keys():
+    print(name.title())
+
+friends = ['phil', 'sarah']
+
+for name in favorite_languages.keys():
+    print(f"Hi {name.title()}")
+
+    if name in friends:
+        language = favorite_languages[name].title()
+        print(f"\t{name.title()}, I see you love {language}!")
+
+
